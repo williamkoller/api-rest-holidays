a7de26d6fea341b92e9df8a049b69b9b731ace9b
William Koller <williamkoller404@gmail.com>
2022-06-05 22:54:34 -0300

    chore: remove mongoose

--- src/modules/app/app.settings.ts
+++ src/modules/app/app.settings.ts
@@ -1,18 +1,10 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import envFolderPath, { envs } from '@/config/env';
-import { MongooseModule } from '@nestjs/mongoose';
 
 export const imports = [
   ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: envFolderPath.folderPath,
     load: [envs],
-  }),
-  MongooseModule.forRootAsync({
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: async (config: ConfigService) => ({
-      uri: config.get<string>('mongoUri'),
-    }),
   }),
 ];
