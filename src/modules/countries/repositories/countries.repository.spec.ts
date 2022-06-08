5a613abf73ac0bf287b99afbc29c2e062163af10
William Koller <williamkoller404@gmail.com>
2022-06-08 09:48:12 -0300

    test: should be called save with correct params

--- src/modules/countries/repositories/countries.repository.spec.ts
+++ src/modules/countries/repositories/countries.repository.spec.ts
@@ -1,8 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Country } from '../models/country.model';
 import { CountriesRepository } from './countries.repository';
 
 describe('CountriesRepository', () => {
   let repository: CountriesRepository;
+  let mockAddCountry: Country;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,8 +12,26 @@ describe('CountriesRepository', () => {
     }).compile();
 
     repository = module.get<CountriesRepository>(CountriesRepository);
+
+    mockAddCountry = {
+      _id: 'any_id',
+      name: 'any_name',
+      slug: 'any_slug',
+      createdAt: new Date('2022-06-08'),
+      updatedAt: new Date('2022-06-08'),
+    };
+
+    repository.save = jest.fn();
   });
   it('should be defined', () => {
     expect(repository).toBeDefined();
+  });
+
+  describe('addCountry()', () => {
+    it('should be called save with correct params', async () => {
+      repository.save = jest.fn().mockReturnValue(mockAddCountry);
+      await repository.addCountry(mockAddCountry);
+      expect(repository.save).toBeCalledWith(mockAddCountry);
+    });
   });
 });
