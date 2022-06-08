462ac16703170a7fd204720547cd5392aefe910a
William Koller <williamkoller404@gmail.com>
2022-06-08 09:47:58 -0300

    feat: add method addCountry

--- src/modules/countries/repositories/countries.repository.ts
+++ src/modules/countries/repositories/countries.repository.ts
@@ -1,5 +1,11 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Country } from '@/modules/countries/models/country.model';
+import { AddCountryDto } from '../dtos/add-country/add-country.dto';
 
 EntityRepository(Country);
-export class CountriesRepository extends Repository<Country> {}
+export class CountriesRepository extends Repository<Country> {
+  async addCountry(addCountryDto: AddCountryDto): Promise<Country> {
+    const newCountry = Object.assign({} as AddCountryDto, addCountryDto);
+    return await this.save(newCountry);
+  }
+}
