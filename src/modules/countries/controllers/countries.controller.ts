aa175a4f1a122dd9f355ffe25717c1c573136243
William Koller <williamkoller404@gmail.com>
2022-06-09 22:37:59 -0300

    feat: add addCountry in controller

--- src/modules/countries/controllers/countries.controller.ts
+++ src/modules/countries/controllers/countries.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AddCountryDto } from '@/modules/countries/dtos/add-country/add-country.dto';
+import { Country } from '@/modules/countries/models/country.model';
+import { AddCountryService } from '@/modules/countries/services/add-country/add-country.service';
 
 @Controller('countries')
-export class CountriesController {}
+export class CountriesController {
+  constructor(private readonly addCountryService: AddCountryService) {}
+
+  @Post()
+  async add(addCountryDto: AddCountryDto): Promise<Country> {
+    return await this.addCountryService.addCountry(addCountryDto);
+  }
+}
