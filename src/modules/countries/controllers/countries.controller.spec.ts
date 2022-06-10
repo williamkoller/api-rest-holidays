321f6707d4db2bb729f78d2a9790ebb968a4f092
William Koller <williamkoller404@gmail.com>
2022-06-10 11:49:52 -0300

    test: should be returns when service returns

--- src/modules/countries/controllers/countries.controller.spec.ts
+++ src/modules/countries/controllers/countries.controller.spec.ts
@@ -3,10 +3,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { Country } from '@/modules/countries/entities/country.entity';
 import { AddCountryService } from '../services/add-country/add-country.service';
 import { CountriesController } from './countries.controller';
+import { FindAllCountriesService } from '../services/find-all-countries/find-all-contries.service';
 
 describe('CountriesController', () => {
   let controller: CountriesController;
   let service: AddCountryService;
+  let findAllService: FindAllCountriesService;
   let mockAddCountry: Country;
 
   beforeEach(async () => {
@@ -20,10 +22,19 @@ describe('CountriesController', () => {
           provide: AddCountryService,
           useFactory: () => mockService,
         },
+        {
+          provide: FindAllCountriesService,
+          useValue: {
+            findAll: jest.fn(),
+          },
+        },
       ],
     }).compile();
     controller = module.get<CountriesController>(CountriesController);
     service = module.get<AddCountryService>(AddCountryService);
+    findAllService = module.get<FindAllCountriesService>(
+      FindAllCountriesService,
+    );
 
     mockAddCountry = {
       _id: 'any_id',
@@ -38,17 +49,26 @@ describe('CountriesController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should be throw when service throw', async () => {
-    (service.addCountry as jest.Mock).mockRejectedValue(
-      new ConflictException('there is already a country with that name.'),
-    );
-    await expect(controller.add(mockAddCountry)).rejects.toThrow(
-      new ConflictException('there is already a country with that name.'),
-    );
+  describe('add()', () => {
+    it('should be throw when service throw', async () => {
+      (service.addCountry as jest.Mock).mockRejectedValue(
+        new ConflictException('there is already a country with that name.'),
+      );
+      await expect(controller.add(mockAddCountry)).rejects.toThrow(
+        new ConflictException('there is already a country with that name.'),
+      );
+    });
+
+    it('should be returns when service returns', async () => {
+      (service.addCountry as jest.Mock).mockReturnValue(mockAddCountry);
+      expect(await controller.add(mockAddCountry)).toEqual(mockAddCountry);
+    });
   });
 
-  it('should be returns when service returns', async () => {
-    (service.addCountry as jest.Mock).mockReturnValue(mockAddCountry);
-    expect(await controller.add(mockAddCountry)).toEqual(mockAddCountry);
+  describe('findAll()', () => {
+    it('should be returns when service returns', async () => {
+      (findAllService.findAll as jest.Mock).mockReturnValue(mockAddCountry);
+      expect(await controller.find()).toEqual(mockAddCountry);
+    });
   });
 });
