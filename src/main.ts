a82d4f7bf5ca9728e5cd329f2e9f049a67c077db
William Koller <williamkoller404@gmail.com>
2022-06-10 14:08:53 -0300

    chore: add api version prefix

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ async function bootstrap() {
   const logger = new Logger('Main');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  app.setGlobalPrefix('api/v1');
+
   const config = app.get<ConfigService>(ConfigService);
   const port = config.get<string>('port');
   const nodeEnv = config.get<string>('nodeEnv');
