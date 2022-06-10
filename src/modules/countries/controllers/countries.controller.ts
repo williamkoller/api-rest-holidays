9e20003e210bd7b70b9a8c288309c202a1be9e6b
William Koller <williamkoller404@gmail.com>
2022-06-10 11:50:07 -0300

    feat: add findAllService

--- src/modules/countries/controllers/countries.controller.ts
+++ src/modules/countries/controllers/countries.controller.ts
@@ -1,14 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AddCountryDto } from '@/modules/countries/dtos/add-country/add-country.dto';
 import { Country } from '@/modules/countries/entities/country.entity';
 import { AddCountryService } from '@/modules/countries/services/add-country/add-country.service';
+import { FindAllCountriesService } from '../services/find-all-countries/find-all-contries.service';
 
 @Controller('countries')
 export class CountriesController {
-  constructor(private readonly addCountryService: AddCountryService) {}
+  constructor(
+    private readonly addCountryService: AddCountryService,
+    private readonly findAllCountriesService: FindAllCountriesService,
+  ) {}
 
   @Post()
   async add(@Body() addCountryDto: AddCountryDto): Promise<Country> {
     return await this.addCountryService.addCountry(addCountryDto);
+  }
+
+  @Get()
+  async find(): Promise<Country[]> {
+    return await this.findAllCountriesService.findAll();
   }
 }
