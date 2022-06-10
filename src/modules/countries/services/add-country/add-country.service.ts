bcc31806a6b48d721196cca7735c80e6718c2c00
William Koller <williamkoller404@gmail.com>
2022-06-09 22:15:35 -0300

    feat: add validation if already a country with that name

--- src/modules/countries/services/add-country/add-country.service.ts
+++ src/modules/countries/services/add-country/add-country.service.ts
@@ -1,7 +1,19 @@
-import { InternalServerErrorException } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { AddCountryDto } from '../../dtos/add-country/add-country.dto';
+import { CountriesRepository } from '../../repositories/countries.repository';
 
+@Injectable()
 export class AddCountryService {
-  async addCountry() {
-    throw new InternalServerErrorException();
+  constructor(private readonly countriesRepo: CountriesRepository) {}
+  async addCountry(addCountryDto: AddCountryDto) {
+    const countryFound = await this.countriesRepo.findByName(
+      addCountryDto.name,
+    );
+
+    if (countryFound) {
+      throw new ConflictException('there is already a country with that name.');
+    }
+
+    return await this.countriesRepo.addCountry(addCountryDto);
   }
 }
