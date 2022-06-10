62aeef14f9e17b1f8ead4b8920a3a79e702db351
William Koller <williamkoller404@gmail.com>
2022-06-10 01:12:46 -0300

    chore: add semicolon

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/modules/app/app.module';
-import { NestExpressApplication } from '@nestjs/platform-express'
-import { Logger } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const logger = new Logger('Main')
+  const logger = new Logger('Main');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
+
   const config = app.get<ConfigService>(ConfigService);
   const port = config.get<string>('port');
   const nodeEnv = config.get<string>('nodeEnv');
