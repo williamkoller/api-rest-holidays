2bab1deab2c944b16c7e2c966d1eb5945e28f8f9
William Koller <williamkoller404@gmail.com>
2022-06-09 21:53:10 -0300

    test: should be throw if repository throw

--- src/modules/countries/services/add-country/add-country.service.spec.ts
+++ src/modules/countries/services/add-country/add-country.service.spec.ts
@@ -1,10 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CountriesRepository } from '@/modules/countries/repositories/countries.repository';
 import { AddCountryService } from './add-country.service';
+import { Country } from '../../models/country.model';
+import { InternalServerErrorException } from '@nestjs/common';
 
 describe('AddCountryService', () => {
   let repository: CountriesRepository;
   let service: AddCountryService;
+  let mockAddCountry: Country;
+
   beforeEach(async () => {
     const countriesRepositoryMock = {
       addCountry: jest.fn(),
@@ -20,6 +24,14 @@ describe('AddCountryService', () => {
     }).compile();
     service = module.get<AddCountryService>(AddCountryService);
     repository = module.get<CountriesRepository>(CountriesRepository);
+
+    mockAddCountry = {
+      _id: 'any_id',
+      name: 'any_name',
+      slug: 'any_slug',
+      createdAt: new Date('2022-06-08'),
+      updatedAt: new Date('2022-06-08'),
+    };
   });
   it('should be service defined', () => {
     expect(service).toBeDefined();
@@ -27,5 +39,13 @@ describe('AddCountryService', () => {
 
   it('should be repository defined', () => {
     expect(repository).toBeDefined();
+  });
+
+  describe('addCountry()', () => {
+    it('should be throw if repository throw', async () => {
+      await expect(service.addCountry()).rejects.toThrow(
+        new InternalServerErrorException(),
+      );
+    });
   });
 });
