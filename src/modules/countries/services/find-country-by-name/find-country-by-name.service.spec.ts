439b80eea43545f74a27e65bd7c46e17a135a170
William Koller <williamkoller404@gmail.com>
2022-06-19 18:40:16 -0300

    test: should be no throw if repository returns

--- src/modules/countries/services/find-country-by-name/find-country-by-name.service.spec.ts
+++ src/modules/countries/services/find-country-by-name/find-country-by-name.service.spec.ts
@@ -1,16 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Country } from '../../entities/country.entity';
+import { CountriesRepository } from '../../repositories/countries.repository';
 import { FindCountryByNameService } from './find-country-by-name.service';
 
 describe('FindCountryByNameService Test', () => {
   let service: FindCountryByNameService;
+  let repository: CountriesRepository;
+  let mockAddCountry: Country;
+
   beforeEach(async () => {
+    const countriesRepositoryMock = {
+      findByName: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FindCountryByNameService],
+      providers: [
+        FindCountryByNameService,
+        {
+          provide: CountriesRepository,
+          useFactory: () => countriesRepositoryMock,
+        },
+      ],
     }).compile();
 
     service = module.get<FindCountryByNameService>(FindCountryByNameService);
+    repository = module.get<CountriesRepository>(CountriesRepository);
+    mockAddCountry = {
+      _id: 'any_id',
+      name: 'any_name',
+      slug: 'any_slug',
+      createdAt: new Date('2022-06-08'),
+      updatedAt: new Date('2022-06-08'),
+    };
   });
-  it('should be defined', () => {
+  it('should be service defined', () => {
     expect(service).toBeDefined();
+  });
+  it('should be repository defined', () => {
+    expect(repository).toBeDefined();
+  });
+
+  describe('findByName()', () => {
+    it('should be no throw if repository returns', async () => {
+      await expect(service.findByName('any_name')).resolves.not.toThrow();
+    });
   });
 });
