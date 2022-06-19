cfe95df78a832325a61d86bcd15dd638ef60a5f6
William Koller <williamkoller404@gmail.com>
2022-06-19 18:40:56 -0300

    feat: add FindCountryByNameService

--- src/modules/countries/services/find-country-by-name/find-country-by-name.service.ts
+++ src/modules/countries/services/find-country-by-name/find-country-by-name.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Country } from '../../entities/country.entity';
+import { CountriesRepository } from '../../repositories/countries.repository';
 
 @Injectable()
-export class FindCountryByNameService {}
+export class FindCountryByNameService {
+  constructor(private readonly countriesRepo: CountriesRepository) {}
+
+  async findByName(name: string): Promise<unknown> {
+    return name;
+  }
+}
