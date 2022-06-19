dfa517538eabf2c54591caf6c66b9f14aa0c94ba
William Koller <williamkoller404@gmail.com>
2022-06-19 18:41:14 -0300

    feat: update settings

--- src/modules/countries/countries.settings.ts
+++ src/modules/countries/countries.settings.ts
@@ -4,12 +4,14 @@ import { Country } from '@/modules/countries/entities/country.entity';
 import { CountriesRepository } from './repositories/countries.repository';
 import { AddCountryService } from './services/add-country/add-country.service';
 import { FindAllCountriesService } from './services/find-all-countries/find-all-contries.service';
+import { FindCountryByNameService } from './services/find-country-by-name/find-country-by-name.service';
 
 export const imports = [TypeOrmModule.forFeature([Country])];
 export const providers = [
   AddCountryService,
   CountriesRepository,
   FindAllCountriesService,
+  FindCountryByNameService,
 ];
 export const controllers = [CountriesController];
 export const exportings = [AddCountryService];
